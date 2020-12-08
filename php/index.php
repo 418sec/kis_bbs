5d4ec61294cdbdf4ec02970b837b4743d0f6f343
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-12-09 04:19:06 +0900

    index.php require post.html

--- php/index.php
+++ php/index.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>つなぐ会</title>
-</head>
-<body>
-  
-  <form method="post" action="/post_notification.php">
-    ニックネームを入力してください．<br />
-    <input name="nickname" type="text" style="width:100px">
-    <br />
-    <br />
-    <input type="submit" value="送信">
-  </form>
-  
-  <?php
-    require (dirname (__FILE__) . '/post_notification.php');
-  ?>
-  
-</body>
-</html>
-
-
+<?php
+  require (dirname (__FILE__) . '/html/post.html');
+?>
