1208de4f966033f8d46a078b6b8fa9e0818079d0
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-12-09 04:22:00 +0900

    index.php update

--- php/index.php
+++ php/index.php
@@ -1,3 +1,14 @@
-<?php
-  require (dirname (__FILE__) . '/html/post.html');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>つなぐ会</title>
+</head>
+<body>
+
+  <?php
+    require (dirname (__FILE__) . '/html/post.html');
+  ?>
+
+</body>
+</html>
