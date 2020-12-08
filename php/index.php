e3e7a0586f8f31dc5f0997a51b2516e9ffb888e3
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-12-09 04:02:31 +0900

    index.php

--- php/index.php
+++ php/index.php
@@ -6,6 +6,14 @@
 </head>
 <body>
   
+  <form method="post" action="/post_notification.php">
+    ニックネームを入力してください．<br />
+    <input name="nickname" type="text" style="width:100px">
+    <br />
+    <br />
+    <input type="submit" value="送信">
+  </form>
+  
   <?php
     require (dirname (__FILE__) . '/post_notification.php');
   ?>
