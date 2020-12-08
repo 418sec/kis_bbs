d45b7e9704f695b9db33948364aca4e915acd11f
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-12-09 03:49:59 +0900

    index.php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>つなぐ会</title>
+</head>
+<body>
+  
+  <?php
+    require (dirname (__FILE__) . '/post_notification.php');
+  ?>
+  
+</body>
+</html>
 
-require (dirname (__FILE__) . '/post_notification.php');
 
-?>
