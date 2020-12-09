4186edd7a51bb802ef2907868ce97e17570159fb
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-12-09 22:31:11 +0900

    thank.php

--- php/thanks.php
+++ php/thanks.php
@@ -7,7 +7,22 @@
 <body>
 
   <?php
-    echo 'ありがとうございます！';
+    $nickname = $_POST['nickname'];
+    $category = $_POST['category'];
+    $question = $_POST['question'];
+
+    $nickname = htmlspecialchars($nickname);
+    $category = htmlspecialchars($category);
+    $question = htmlspecialchars($question);
+
+    echo $nickname;
+    echo '様<br />';
+    echo '質問ありがとうございます<br />';
+    echo '頂いた質問『';
+    echo $question;
+    echo '』<br />';
+    echo $email;
+    echo 'にメールを送信しましたので，ご確認ください';
   ?>
 
 </body>
