7f0222d48029ef5bb0e76e37c35aa952f5ddc8d4
parsecmonkey <daisuke-1120@i.softbank.jp>
2020-12-09 18:47:34 +0900

    post.html post_notificaation.php更新

--- php/post_notification.php
+++ php/post_notification.php
@@ -1,11 +1,58 @@
-<!-- 
+<!--
     質問ページからの質問を始めに受け取るファイル．
     post_database.pyとpost_slack_bot.py
     を動かす
  -->
- 
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>つなぐ会</title>
+</head>
+<body>
 
-echo "post_notification.php";
+  <?php
+    $nickname = $_POST['nickname'];
+    $category = $_POST['category'];
+    $question = $_POST['question'];
 
-?>
+    if ($nickname == '') {
+      echo 'ニックネームが入力されていません<br/>';
+    } else {
+      echo 'ようこそ';
+      echo $nickname . '様';
+      echo '<br/>';
+    }
+
+    if ($category == '') {
+      echo 'カテゴリーが入力されていません<br/>';
+    } else {
+      echo 'カテゴリー：';
+      echo $category;
+      echo '<br/>';
+    }
+
+    if ($question == '') {
+      echo '質問が入力されていません<br/>';
+    } else {
+      echo '質問内容『';
+      echo $question;
+      echo '』<br/>';
+    }
+
+    echo '<br/>';
+
+    if ($nickname == '' || $category == '' || $question == '') {
+      echo '<form>';
+      echo '<input type="button" onclick="history.back()" value="戻る">';
+      echo '</form>';
+    } else {
+      echo '<form method="post" action="thanks.php">';
+      echo '<input type="button" onclick="history.back()" value="戻る">';
+      echo '<input type="submit" value="OK">';
+      echo '</form>';
+    }
+  ?>
+
+</body>
+</html>
